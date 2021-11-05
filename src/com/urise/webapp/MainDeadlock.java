7bab1ecfdf6f595179ae4c034578401b6082f4b1
Ilya20011982 <Ilya_fr@mail.ru>
2021-11-05 19:29:30 +0500

    hw_11 with corrections

---
+++ src/com/urise/webapp/MainDeadlock.java
@@ -0,0 +1,29 @@
+package com.urise.webapp;
+
+public class MainDeadlock {
+    public static void main(String[] args) {
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
+
+    }
+
+    private static void deadLock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println("Waiting " + lock1 + " " + Thread.currentThread().getName());
+            synchronized (lock1) {
+                System.out.println("Holding " + lock1 + " " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " + lock2 + " " + Thread.currentThread().getName());
+                synchronized (lock2) {
+                    System.out.println("Holding " + lock2 + " " + Thread.currentThread().getName());
+                }
+            }
+        }).start();
+    }
+}
