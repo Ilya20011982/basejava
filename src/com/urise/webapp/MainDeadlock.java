c5c0cdf9970e40ca33d0bebf977f5e35671bff7d
Ilya20011982 <Ilya_fr@mail.ru>
2021-11-06 00:11:44 +0500

    hw_11 with corrections ver.2

--- src/com/urise/webapp/MainDeadlock.java
+++ src/com/urise/webapp/MainDeadlock.java
@@ -11,18 +11,18 @@ public class MainDeadlock {
 
     private static void deadLock(Object lock1, Object lock2) {
         new Thread(() -> {
-            Thread thread = Thread.currentThread();
-            System.out.println("Waiting " + lock1 + " " + thread.getName());
+            String thread = Thread.currentThread().getName();
+            System.out.println("Waiting " + lock1 + " " + thread);
             synchronized (lock1) {
-                System.out.println("Holding " + lock1 + " " + thread.getName());
+                System.out.println("Holding " + lock1 + " " + thread);
                 try {
                     Thread.sleep(50);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("Waiting " + lock2 + " " + thread.getName());
+                System.out.println("Waiting " + lock2 + " " + thread);
                 synchronized (lock2) {
-                    System.out.println("Holding " + lock2 + " " + thread.getName());
+                    System.out.println("Holding " + lock2 + " " + thread);
                 }
             }
         }).start();
